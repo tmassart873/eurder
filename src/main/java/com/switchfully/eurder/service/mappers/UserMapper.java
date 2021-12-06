65288811d44758d034f22f676977c4709809d674
tmassart873 <tim.massart@hotmail.com>
2021-12-06 16:32:14 +0100

    created UserMapper class

---
+++ src/main/java/com/switchfully/eurder/service/mappers/UserMapper.java
@@ -0,0 +1,37 @@
+package com.switchfully.eurder.service.mappers;
+
+import com.switchfully.eurder.domain.user.User;
+import com.switchfully.eurder.service.dtos.userdto.CreateUserDto;
+import com.switchfully.eurder.service.dtos.userdto.UserDto;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserMapper {
+    public User mapCreateUserDtoToUser(CreateUserDto createUserDto) {
+        return User.UserBuilder.userBuilder()
+
+                .withFirstName(createUserDto.getFirstName())
+                .withLastName(createUserDto.getLastName())
+                .withEmailAddress(createUserDto.getEmailAddress())
+                .withAddress(createUserDto.getAddress())
+                .withPhoneNumber(createUserDto.getPhoneNumber())
+                .withPassword(createUserDto.getPassword())
+                .withRole(createUserDto.getRole())
+                .build();
+    }
+
+    public UserDto mapUserToUserDto(User user) {
+
+        return new UserDto()
+                .setId(user.getId())
+                .setFirstName(user.getFirstName())
+                .setLastName(user.getLastName())
+                .setEmailAddress(user.getEmailAddress())
+                .setAddress(user.getAddress())
+                .setPhoneNumber(user.getPhoneNumber())
+                .setPassword(user.getPassword())
+                .setRole(user.getRole());
+
+
+    }
+}
