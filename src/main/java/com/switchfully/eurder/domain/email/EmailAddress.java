d57d595176504226ff830923b273e21ca335f5d0
tmassart873 <tim.massart@hotmail.com>
2021-12-06 16:32:48 +0100

    serialized EmailAddress class

--- src/main/java/com/switchfully/eurder/domain/email/EmailAddress.java
+++ src/main/java/com/switchfully/eurder/domain/email/EmailAddress.java
@@ -1,7 +1,17 @@
 package com.switchfully.eurder.domain.email;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.switchfully.eurder.domain.address.Address;
+
+import java.util.Objects;
+
+@JsonSerialize
 public class EmailAddress {
+    @JsonProperty("username")
     private final String username;
+    @JsonProperty("domain")
     private final String domain;
 
     public EmailAddress(String username, String domain) {
@@ -9,7 +19,16 @@ public class EmailAddress {
         this.domain = domain;
     }
 
-    public String getFullEmailAddress(){
+    @Override
+    public String toString() {
         return this.username + "@" + this.domain;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        EmailAddress that = (EmailAddress) o;
+        return Objects.equals(username, that.username) && Objects.equals(domain, that.domain);
     }
 }
