f519e455318b5f8af807ee7f9e675f0bef2d1c51
tmassart873 <tim.massart@hotmail.com>
2021-12-06 16:33:02 +0100

    added features to every role

--- src/main/java/com/switchfully/eurder/domain/user/Role.java
+++ src/main/java/com/switchfully/eurder/domain/user/Role.java
@@ -1,6 +1,20 @@
 package com.switchfully.eurder.domain.user;
 
+import com.switchfully.eurder.domain.Feature;
+
+import java.util.List;
+
 public enum Role {
-    CUSTOMER,
-    ADMIN
+    CUSTOMER(List.of(Feature.ORDER_ITEMS)),
+    ADMIN(List.of(Feature.ORDER_ITEMS,Feature.ADD_NEW_ITEM,Feature.REGISTER_ADMIN));
+
+    private List<Feature> featureList;
+
+    Role(List<Feature> featureList) {
+        this.featureList = featureList;
+    }
+    public boolean containsFeature(Feature feature) {
+        return featureList.contains(feature);
+    }
 }
+
