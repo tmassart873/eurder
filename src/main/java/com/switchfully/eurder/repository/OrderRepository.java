cce291135ac02e3a02b7f1d8b6e290d5a0301cc0
tmassart873 <tim.massart@hotmail.com>
2021-12-07 16:06:35 +0100

    added hasmap for customers orders

--- src/main/java/com/switchfully/eurder/repository/OrderRepository.java
+++ src/main/java/com/switchfully/eurder/repository/OrderRepository.java
@@ -1,24 +1,28 @@
 package com.switchfully.eurder.repository;
 
-import com.switchfully.eurder.domain.item.Item;
 import com.switchfully.eurder.domain.order.Order;
+import org.springframework.stereotype.Repository;
 
 import java.util.HashMap;
 import java.util.List;
 import java.util.UUID;
 
+@Repository
 public class OrderRepository {
     private HashMap<UUID, Order> orders = new HashMap<>();
+    //HashMap<customerId,orderId>
+    private HashMap<UUID, UUID> ordersByCustomerId = new HashMap<>();
 
-    public OrderRepository() {}
+    public OrderRepository() {
+    }
 
     public Order save(Order order) {
         orders.put(order.getOrderId(), order);
+        ordersByCustomerId.put(order.getCustomerID(),order.getOrderId());
         return order;
     }
 
-    public List<Order> getAllItems(){
+    public List<Order> getAllOrders() {
         return orders.values().stream().toList();
     }
-
 }
