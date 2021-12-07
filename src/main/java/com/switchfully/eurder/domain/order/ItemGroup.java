bc55eb64970f8ff953ce82352f46bc8a54d72e18
tmassart873 <tim.massart@hotmail.com>
2021-12-07 16:09:25 +0100

    change id to String (story 3)

--- src/main/java/com/switchfully/eurder/domain/order/ItemGroup.java
+++ src/main/java/com/switchfully/eurder/domain/order/ItemGroup.java
@@ -2,39 +2,25 @@ package com.switchfully.eurder.domain.order;
 
 import com.switchfully.eurder.domain.item.Item;
 import com.switchfully.eurder.repository.ItemRepository;
-import org.apache.tomcat.jni.Local;
 
 import java.time.LocalDate;
 import java.util.UUID;
 
 public class ItemGroup {
-    private UUID itemId;
+    private String itemId;
     private int amount;
     private LocalDate shippingDate;
-    private ItemRepository itemRepository;
 
-    public ItemGroup(UUID itemId, int amount) {
+    public ItemGroup(String itemId, int amount) {
         this.itemId = itemId;
         this.amount = amount;
-        this.shippingDate = calculateShippingDate(itemId);
     }
 
-    private LocalDate calculateShippingDate(UUID itemId) {
-        Item item = itemRepository.getItemById(itemId);
-        if(item.isAvailable())
-            return LocalDate.now().plusDays(1);
-        return LocalDate.now().plusDays(7);
-    }
-    public double calculateItemTotalCost(){
-        double price = itemRepository.getItemById(itemId).getPrice();
-        return amount * price;
-    }
-
-    public UUID getItemId() {
+    public String getItemId() {
         return itemId;
     }
 
-    public ItemGroup setItemId(UUID itemId) {
+    public ItemGroup setItemId(String itemId) {
         this.itemId = itemId;
         return this;
     }
@@ -54,15 +40,6 @@ public class ItemGroup {
 
     public ItemGroup setShippingDate(LocalDate shippingDate) {
         this.shippingDate = shippingDate;
-        return this;
-    }
-
-    public ItemRepository getItemRepository() {
-        return itemRepository;
-    }
-
-    public ItemGroup setItemRepository(ItemRepository itemRepository) {
-        this.itemRepository = itemRepository;
         return this;
     }
 }
