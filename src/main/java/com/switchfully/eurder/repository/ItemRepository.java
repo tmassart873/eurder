c8c86ebb9fcf6cd2334886015033d8ad47fdc956
tmassart873 <tim.massart@hotmail.com>
2021-12-07 16:09:10 +0100

    change id to String (story 3)

--- src/main/java/com/switchfully/eurder/repository/ItemRepository.java
+++ src/main/java/com/switchfully/eurder/repository/ItemRepository.java
@@ -1,29 +1,28 @@
 package com.switchfully.eurder.repository;
 
 import com.switchfully.eurder.domain.item.Item;
-import com.switchfully.eurder.domain.user.User;
 import org.springframework.stereotype.Repository;
 
 import java.util.HashMap;
 import java.util.List;
-import java.util.UUID;
 
 @Repository
 public class ItemRepository {
-    private HashMap<UUID, Item> items = new HashMap<>();
+    private HashMap<String, Item> items = new HashMap<>();
 
-    public ItemRepository() {}
+    public ItemRepository() {
+    }
 
     public Item save(Item item) {
         items.put(item.getId(), item);
         return item;
     }
 
-    public List<Item> getAllItems(){
+    public List<Item> getAllItems() {
         return items.values().stream().toList();
     }
 
-    public Item getItemById(UUID id){
+    public Item getItemById(String id) {
         return items.get(id);
     }
 
