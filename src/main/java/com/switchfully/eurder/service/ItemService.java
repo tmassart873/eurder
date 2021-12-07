519557d6c1ebfdf4860bd9ba666005a2fe64870c
tmassart873 <tim.massart@hotmail.com>
2021-12-07 16:08:31 +0100

    added methods to reduce and update the stock (story 3)

--- src/main/java/com/switchfully/eurder/service/ItemService.java
+++ src/main/java/com/switchfully/eurder/service/ItemService.java
@@ -1,6 +1,9 @@
 package com.switchfully.eurder.service;
 
 import com.switchfully.eurder.domain.item.Item;
+import com.switchfully.eurder.domain.order.ItemGroup;
+import com.switchfully.eurder.domain.order.Order;
+import com.switchfully.eurder.exceptions.NotEnoughStockException;
 import com.switchfully.eurder.repository.ItemRepository;
 import com.switchfully.eurder.service.dtos.itemdto.CreateItemDto;
 import com.switchfully.eurder.service.dtos.itemdto.ItemDto;
@@ -27,4 +30,18 @@ public class ItemService {
     public List<ItemDto> getAllItems(){
         return itemRepository.getAllItems().stream().map(itemMapper::mapItemToItemDto).toList();
     }
+
+    public void reduceAmountInStock(ItemGroup itemGroup){
+        int currentStock = itemRepository.getItemById(itemGroup.getItemId()).getAmountInStock();
+        int amountOrdered = itemGroup.getAmount();
+        if(amountOrdered > currentStock){
+            throw new NotEnoughStockException();
+        }
+        else
+            itemRepository.getItemById(itemGroup.getItemId()).setAmountInStock(currentStock - amountOrdered);
+    }
+    public void updateStock(Order order){
+        order.getItems().forEach(this::reduceAmountInStock);
+    }
+
 }
