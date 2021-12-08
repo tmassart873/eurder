bcc3cafea9ada6e439966f116ec2a63c550afc46
tmassart873 <tim.massart@hotmail.com>
2021-12-08 12:13:44 +0100

    tested story 1 / 7 / 8

---
+++ src/test/java/com/switchfully/eurder/api/TestApplication.java
@@ -0,0 +1,7 @@
+package com.switchfully.eurder.api;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(scanBasePackages = {"com.switchfully.eurder"})
+public class TestApplication {
+}
--- src/test/java/com/switchfully/eurder/api/UserControllerIntegrationTest.java
+++ src/test/java/com/switchfully/eurder/api/UserControllerIntegrationTest.java
@@ -3,64 +3,131 @@ package com.switchfully.eurder.api;
 import com.switchfully.eurder.domain.address.Address;
 import com.switchfully.eurder.domain.email.EmailAddress;
 import com.switchfully.eurder.domain.user.Role;
+import com.switchfully.eurder.domain.user.User;
+import com.switchfully.eurder.exceptions.UserNotAuthorizedException;
+import com.switchfully.eurder.repository.UserRepository;
 import com.switchfully.eurder.service.dtos.userdto.CreateUserDto;
 import com.switchfully.eurder.service.dtos.userdto.UserDto;
-import io.restassured.RestAssured;
-import io.restassured.http.ContentType;
+import com.switchfully.eurder.service.mappers.UserMapper;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.HttpStatus;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+import java.util.List;
+import java.util.UUID;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.catchThrowable;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+@SpringBootTest(classes = TestApplication.class)
 public class UserControllerIntegrationTest {
 
-    @Value("${server.port}")
-    private int port;
+    @Autowired
+    private UserController userController;
+    @Autowired
+    private UserMapper userMapper = new UserMapper();
+    @Autowired
+    private UserRepository userRepository;
+
+    Address address;
+    EmailAddress emailDirk;
+    EmailAddress emailMarc;
+    CreateUserDto createDirk;
+    User userMarc;
+    User userDirk;
+    UserDto userDtoMarc;
+    static boolean isSetupDone = false;
+
+    @BeforeEach
+    void setUp() {
+        if (!isSetupDone) {
+            initTest();
+            userController.createNewUser(createDirk, "Basic ");
+            userDirk = userRepository.getUserByEmail("dirk@order.com");
+            isSetupDone = true;
+        }
+    }
+
+    //Story 1 : Create a customer account
+    @Test
+    void registerANewUser_whenPostANewUser_thenUserInUserRepository() {
+        //Given
+
+        //When
+        userDirk = userRepository.getUserByEmail("dirk@order.com");
+
+        //Then
+        Assertions.assertTrue(userRepository.getAllUsers().contains(userDirk));
+        Assertions.assertFalse(userRepository.getAllUsers().contains(userMarc));
+    }
+
+    //Story 7 : View all customers
+    @Test
+    void givenAdmin_getAllCustomers_thenReturnsAllTheCustomers() {
+        //Given
+
+        //When
+        List<UserDto> actualList = userController.getAllCustomers("Basic YWRtaW4udG1Ab3JkZXIuY29tOmFkbWluX3Rt");
+        List<UserDto> expectedList = userRepository.getAllUsers().stream().filter(user -> user.getRole().equals(Role.CUSTOMER)).map(user -> userMapper.mapUserToUserDto(user)).toList();
+
+        //Then
+        Assertions.assertTrue(actualList.containsAll(expectedList));
+    }
+
+    //Story 7 : View all customers
+    @Test
+    void givenCustomer_getAllCustomers_thenThrowException() {
+        //Given
+
+        //When
+        Throwable exception = catchThrowable(() -> userController.getAllCustomers("Basic ZGlya0BvcmRlci5jb206ZGlyaw=="));
+
+        //Then
+        assertThat(exception).isInstanceOf(UserNotAuthorizedException.class).hasMessage("You are not allowed to do this action.");
+    }
 
     @Test
-    void registerANewUser_whenPostANewUser_thenCreateUserInUserRepository() {
+    void givenAdmin_getACustomers_thenReturnsASpecificCustomer() {
         //Given
-        Address address = Address.AddressBuilder.addressBuilder()
+        UUID id = userDirk.getId();
+
+        //When
+        UserDto actualUserDto = userController.getSingleCustomer(id.toString(),"Basic YWRtaW4udG1Ab3JkZXIuY29tOmFkbWluX3Rt");
+        UserDto expectedUserDto = userMapper.mapUserToUserDto(userDirk);
+
+        //Then
+        Assertions.assertEquals(actualUserDto,expectedUserDto);
+    }
+
+    public void initTest() {
+        address = Address.AddressBuilder.addressBuilder()
                 .withStreetName("Veldstraat")
                 .withStreetNumber("68")
                 .withPostCode("9000")
                 .withCity("Ghent")
                 .build();
-        EmailAddress emailAddress = new EmailAddress("tm", "order.com");
-        CreateUserDto createUserDto = new CreateUserDto()
-                .setAddress(address)
-                .setEmailAddress(emailAddress)
-                .setFirstName("T")
+        emailDirk = new EmailAddress("dirk", "order.com");
+        emailMarc = new EmailAddress("marc", "order.com");
+        createDirk = new CreateUserDto()
+                .setFirstName("Dirk")
                 .setLastName("M")
-                .setRole(Role.CUSTOMER)
-                .setPassword("tm")
-                .setPhoneNumber("789641230");
-
-        //When
-        UserDto userDto = RestAssured
-                .given()
-                .body(createUserDto)
-                .accept(ContentType.JSON)
-                .contentType(ContentType.JSON)
-                .when()
-                .port(port)
-                .post("/users")
-                .then()
-                .assertThat()
-                .statusCode(HttpStatus.CREATED.value())
-                .extract()
-                .as(UserDto.class);
-        //Then
-        Assertions.assertEquals(address, userDto.getAddress());
-        Assertions.assertEquals(emailAddress, userDto.getEmailAddress());
-        Assertions.assertEquals("T", userDto.getFirstName());
-        Assertions.assertEquals("M", userDto.getLastName());
-        Assertions.assertEquals("789641230", userDto.getPhoneNumber());
-        Assertions.assertEquals(Role.CUSTOMER, userDto.getRole());
-        Assertions.assertEquals("tm", userDto.getPassword());
-
-
+                .setAddress(address)
+                .setEmailAddress(emailDirk)
+                .setPassword("dirk")
+                .setPhoneNumber("789641230")
+                .setRole(Role.CUSTOMER);
+        userMarc = User.UserBuilder.userBuilder()
+                .withFirstName("Marc")
+                .withLastName("M")
+                .withEmailAddress(emailMarc)
+                .withAddress(address)
+                .withPhoneNumber("951357826")
+                .withRole(Role.CUSTOMER)
+                .build();
+        userDtoMarc = userMapper.mapUserToUserDto(userMarc);
     }
 }
