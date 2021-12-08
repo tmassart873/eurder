a9d44b913ef0f4c9d1a350c451e237de6c08994e
tmassart873 <tim.massart@hotmail.com>
2021-12-08 09:18:51 +0100

    added extra features (story 8 : View a single customer)

--- src/main/java/com/switchfully/eurder/domain/Feature.java
+++ src/main/java/com/switchfully/eurder/domain/Feature.java
@@ -1,5 +1,9 @@
 package com.switchfully.eurder.domain;
 
 public enum Feature {
-    ADD_NEW_ITEM,ORDER_ITEMS,REGISTER_ADMIN
+    ADD_NEW_ITEM,
+    ORDER_ITEMS,
+    REGISTER_ADMIN,
+    CHECK_CUSTOMERS,
+    CHECK_SINGLE_CUSTOMER
 }
