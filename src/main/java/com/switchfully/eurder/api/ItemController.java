dfaaeae4181c8b52574efccdae924c9fdbefeb69
tmassart873 <tim.massart@hotmail.com>
2021-12-08 16:15:42 +0100

    added itemRepo and mapper (story 4)

--- src/main/java/com/switchfully/eurder/api/ItemController.java
+++ src/main/java/com/switchfully/eurder/api/ItemController.java
@@ -1,10 +1,14 @@
 package com.switchfully.eurder.api;
 
 import com.switchfully.eurder.domain.Feature;
+import com.switchfully.eurder.domain.item.Item;
+import com.switchfully.eurder.repository.ItemRepository;
 import com.switchfully.eurder.service.ItemService;
 import com.switchfully.eurder.service.SecurityService;
 import com.switchfully.eurder.service.dtos.itemdto.CreateItemDto;
 import com.switchfully.eurder.service.dtos.itemdto.ItemDto;
+import com.switchfully.eurder.service.dtos.itemdto.UpdateItemDto;
+import com.switchfully.eurder.service.mappers.ItemMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
@@ -17,12 +21,15 @@ import java.util.List;
 @RequestMapping("/items")
 public class ItemController {
     private final ItemService itemService;
+    private final ItemRepository itemRepository;
+    private ItemMapper itemMapper = new ItemMapper();
     private final SecurityService securityService;
     private final Logger myLogger = LoggerFactory.getLogger(UserController.class);
 
-    public ItemController(ItemService itemService, SecurityService securityService) {
+    public ItemController(ItemService itemService, SecurityService securityService,ItemRepository itemRepository) {
         this.itemService = itemService;
         this.securityService = securityService;
+        this.itemRepository = itemRepository;
     }
     @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     @ResponseStatus(HttpStatus.OK)
@@ -34,5 +41,18 @@ public class ItemController {
     public ItemDto addNewItem(@RequestBody CreateItemDto createItemDto,@RequestHeader String authorization){
         securityService.validateAccess(authorization, Feature.ADD_NEW_ITEM);
         return itemService.addNewItem(createItemDto);
+    }
+    @PutMapping(path = "/{id}",consumes = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseStatus(HttpStatus.OK)
+    public ItemDto updateItem(@PathVariable String id, @RequestBody UpdateItemDto updateItemDto, @RequestHeader String authorization){
+        Item item = itemRepository.getItemById(id);
+        securityService.validateAccess(authorization, Feature.UPDATE_ITEM);
+        item
+                .setAmountInStock(updateItemDto.getAmountInStock())
+                .setName(updateItemDto.getName())
+                .setDescription(updateItemDto.getDescription())
+                .setPrice(updateItemDto.getPrice());
+        return itemMapper.mapItemToItemDto(item);
+
     }
 }
