94431d0080d1721a92ca92f9281d4d7606895912
tmassart873 <tim.massart@hotmail.com>
2021-12-13 17:12:13 +0100

    added method getOrdersByCustomerId + removed previous attempt (story 5)

--- src/main/java/com/switchfully/eurder/repository/OrderRepository.java
+++ src/main/java/com/switchfully/eurder/repository/OrderRepository.java
@@ -3,26 +3,25 @@ package com.switchfully.eurder.repository;
 import com.switchfully.eurder.domain.order.Order;
 import org.springframework.stereotype.Repository;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.UUID;
+import java.util.*;
 
 @Repository
 public class OrderRepository {
     private HashMap<UUID, Order> orders = new HashMap<>();
-    //HashMap<customerId,orderId>
-    private HashMap<UUID, UUID> ordersByCustomerId = new HashMap<>();
 
     public OrderRepository() {
     }
 
     public Order save(Order order) {
         orders.put(order.getOrderId(), order);
-        ordersByCustomerId.put(order.getCustomerID(),order.getOrderId());
         return order;
     }
 
     public List<Order> getAllOrders() {
         return orders.values().stream().toList();
+    }
+
+    public List<Order> getOrdersByCustomerId(UUID customerId){
+        return orders.values().stream().filter(order -> order.getCustomerID().equals(customerId)).toList();
     }
 }
